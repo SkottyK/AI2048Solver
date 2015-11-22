394f50811f4f32cd999f047ec3b180cba818fe36
Scott Krulcik <skrulcik@andrew.cmu.edu>
2015-11-21 21:36:45 -0500

    Initial Commit

---
+++ AI2048Solver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AI2048Solver
+//
+//  Created by Scott Krulcik on 11/21/15.
+//  Copyright © 2015 Scott Krulcik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
