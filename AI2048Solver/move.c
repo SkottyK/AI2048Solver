09442b64b6d16652702e055a9644a927363cf8cc
Scott Krulcik <skrulcik@andrew.cmu.edu>
2015-12-06 22:26:52 -0500

    Added move pseudo object

---
+++ AI2048Solver/move.c
@@ -0,0 +1,13 @@
+//
+//  move.c
+//  AI2048Solver
+//
+//  Created by Scott Krulcik on 12/6/15.
+//  Copyright © 2015 Scott Krulcik. All rights reserved.
+//
+
+#include "move.h"
+
+
+
+
---
+++ AI2048Solver/move.h
@@ -0,0 +1,23 @@
+//
+//  move.h
+//  AI2048Solver
+//
+//  Created by Scott Krulcik on 12/6/15.
+//  Copyright © 2015 Scott Krulcik. All rights reserved.
+//
+
+#ifndef move_h
+#define move_h
+
+#include <stdio.h>
+
+#define NUM_MOVES 4
+
+typedef enum move_directions {
+    Up=0,
+    Down,
+    Left,
+    Right
+} Move;
+
+#endif /* move_h */
