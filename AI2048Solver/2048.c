34de09f94ba43046829e80ebf9df1ce78e2e5c1b
sbunting <sbunting96@gmail.com>
2015-12-07 18:15:32 -0500

    start of 2048 file - didn't link Xcode to github until just now

--- AI2048Solver/2048.c
+++ AI2048Solver/2048.c
@@ -5,6 +5,26 @@
 //  Created by Scott Krulcik on 11/24/15.
 //  Copyright © 2015 Scott Krulcik. All rights reserved.
 //
-
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include "2048.h"
+#include "game.h"
+#include "board.h"
 
+void play2048(Game g) {
+    while(!is2048(g->board)) {
+        int* size = (int*)malloc(sizeof(int));
+        Move* moves = effectual_moves(g->board, size);
+        if (moves == NULL) {
+            printf("No more moves, game over!\n");
+        }
+        int i;
+        for (i = 0; i < *size; i++) {
+            
+        }
+        
+        
+        
+    }
+}
