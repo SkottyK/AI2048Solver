7afb809d9116b15672cfb0d84f1627a4208ed7df
sbunting <sbunting96@gmail.com>
2015-12-07 18:19:33 -0500

    fixing

--- AI2048Solver/2048.c
+++ AI2048Solver/2048.c
@@ -11,20 +11,3 @@
 #include "2048.h"
 #include "game.h"
 #include "board.h"
-
-void play2048(Game g) {
-    while(!is2048(g->board)) {
-        int* size = (int*)malloc(sizeof(int));
-        Move* moves = effectual_moves(g->board, size);
-        if (moves == NULL) {
-            printf("No more moves, game over!\n");
-        }
-        int i;
-        for (i = 0; i < *size; i++) {
-            
-        }
-        
-        
-        
-    }
-}
